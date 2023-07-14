2ed37deeb63096f28c67f8c7815022b849095413
devgl96 <georgelucas.dev@gmail.com>
2023-07-13 22:31:26 -0300

    Finish the v1

---
+++ README.md
@@ -0,0 +1,34 @@
+# Notion React App
+
+## Dependencies
+
+- Vite + React + TS
+- Tailwind CSS
+
+## Install dependencies
+
+- pnpm i -D tailwindcss postcss autoprefixer
+- pnpm i @tailwindcss/typography -D
+- pnpm install @tiptap/react @tiptap/pm @tiptap/starter-kit
+- pnpm install lowlight @tiptap/extension-code-block-lowlight highlight.js
+- pnpm i @tiptap/extension-bubble-menu
+- pnpm i react-icons
+- pnpm i @tiptap/extension-floating-menu
+
+### Running commands
+
+- pnpm tailwindcss init -p
+
+### Auxiliar websites
+
+- https://hypercolor.dev/
+- https://tiptap.dev/
+- https://tiptap.dev/api/nodes/code-block-lowlight
+
+### Tips
+
+- Radix UI[https://www.radix-ui.com/]
+- CmdK Paco [https://cmdk.paco.me/]
+- Live blocks [https://liveblocks.io/]
+- Supabase realtime [https://supabase.com/docs/guides/realtime]
+- Yhs [https://github.com/yjs/yjs]
---
+++ src/components/initialContent.ts
@@ -0,0 +1,65 @@
+export const initialContent = `
+      <h2 class="text-2xl font-bold">What is Tailwind CSS?</h2>
+      <p>
+        Tailwind CSS is a utility-first CSS framework that allows you to rapidly
+        build custom user interfaces. It provides you with a set of utility
+        classes that can be combined to create complex designs without writing
+        any CSS from scratch.
+      </p>
+      <p>
+        With Tailwind CSS, you have complete control over your design as it is
+        not opinionated. You can create unique and highly customized interfaces
+        by leveraging the power of utility classes.
+      </p>
+
+      <h2 class="text-2xl font-bold">Getting Started</h2>
+      <p>
+        To get started with Tailwind CSS, you can include the Tailwind CSS CDN
+        in your HTML file:
+      </p>
+
+      <p>
+        You can learn more about this at{" "}
+        <a href="https://rocketseat.com.br">Rocketseat 🚀</a>
+      </p>
+
+      <pre>
+        <code class="language-javascript">const hello = "World";</code>
+      </pre>
+
+      <p>
+        Once you have included the CSS file, you can start using Tailwind CSS
+        classes in your HTML markup.
+      </p>
+
+      <h2 class="text-2xl font-bold">Example Usage</h2>
+      <p>
+        Here's an example of how you can use Tailwind CSS classes to create a
+        simple card component:
+      </p>
+
+      <pre>
+        <code class="language-javascript">console.log("Hello World");</code>
+      </pre>
+
+      <p>
+        In the example above, we've used utility classes like{" "}
+        <code>bg-white</code>, <code>rounded-lg</code>, <code>shadow-lg</code>,{" "}
+        <code>p-6</code>, <code>text-xl</code>, <code>font-bold</code>,{" "}
+        <code>text-gray-800</code>, and <code>text-gray-600</code> to style the
+        card component.
+      </p>
+
+      <h2 class="text-2xl font-bold">Conclusion</h2>
+      <p>
+        Tailwind CSS offers a powerful and flexible approach to building user
+        interfaces. It saves development time by eliminating the need to write
+        custom CSS while providing complete design control. With its extensive
+        set of utility classes, Tailwind CSS enables developers to create
+        visually appealing and responsive designs quickly.
+      </p>
+      <p>
+        So, if you're looking for a CSS framework that offers simplicity,
+        flexibility, and customization, give Tailwind CSS a try!
+      </p>
+    `;
